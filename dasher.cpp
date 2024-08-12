699fe45d6ea6223c3cece5abc10f505612d4b0b4
JesseSipari <sipari.jesse@gmail.com>
2024-08-12 11:37:11 +0300

    Updated nebula hazard animation and did some refactoring

--- dasher.cpp
+++ dasher.cpp
@@ -11,14 +11,29 @@ int main() {
     const int screenHeight{450};    
     const int gravity{1'000}; // Acceleration due to gravity (pixels/s)/s
 
-
-
-
+    // Scarfy variables
     Texture2D scarfy;
     Rectangle scarfyRect;
     Vector2 scarfyPos;
 
-    // animation frame
+
+    InitializeGame(scarfy, scarfyRect, scarfyPos, screenHeight, screenWidth);
+
+    // Nebula variables
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle nebulaRect{0.0f, 0.0f, (float)nebula.width/8, (float)nebula.height/8};
+    Vector2 nebulaPos{screenWidth, screenHeight - nebulaRect.height};
+
+
+    // Nebula X velocity in pixels per second
+    int nebulaVelocity{-400};
+
+    // Variables for nebula animation
+        int nebulaFrame;
+        const float nebulaUpdateTime = 1.0f / 12.0f;
+        float nebulaRunningTime = 0.0f;
+
+    // Animation frame
     int frame{0};
 
     // Time tracking variables for animation
@@ -33,7 +48,6 @@ int main() {
     
    
 
-    InitializeGame(scarfy, scarfyRect, scarfyPos, screenHeight, screenWidth);
 
     // Main game loop
     while (!WindowShouldClose()) {
@@ -41,30 +55,61 @@ int main() {
 
         UpdatePlayer(scarfyPos, scarfyRect, isInAir, velocity, gravity, deltaTime, jumpVelocity);
 
-        // update animation frame
-        if (runningTime >= updateTime) {
+        UpdatePlayerAnimation(runningTime, updateTime, scarfyRect, frame, deltaTime, isInAir);
+
+        DrawGame(scarfy, nebula, scarfyRect, nebulaRect, scarfyPos, nebulaPos);
+
+
+        // Update the nebula position
+        nebulaPos.x += nebulaVelocity * deltaTime;
+
+
+
+        // Update the nebula animation frame
+        if (nebulaRunningTime >= nebulaUpdateTime) {
+            nebulaRunningTime = 0.0f;
+            nebulaRect.x = nebulaFrame * nebulaRect.width;
+            nebulaFrame++;
+
+            if (nebulaFrame > 7) {
+                nebulaFrame = 0;
+            }
+        } else {
+            nebulaRunningTime += deltaTime;
+        }
+    }
+    CleanupGame(scarfy, nebula);
+}
+
+
+
+// Function definitions
+
+void UpdatePlayerAnimation(float &runningTime, const float updateTime, Rectangle &scarfyRect, int &frame, const float deltaTime, bool isInAir)
+{
+    // Only update animation if the player is not in the air
+    if (!isInAir)
+    {
+        // Update animation frame
+        if (runningTime >= updateTime)
+        {
             runningTime = 0.0f;
             scarfyRect.x = frame * scarfyRect.width;
             frame++;
 
-            if (frame > 5) {
+            if (frame > 5)
+            {
                 frame = 0;
-            } 
-        } else {
+            }
+        }
+        else
+        {
             runningTime += deltaTime;
         }
-
-
-
-        DrawGame(scarfy, scarfyRect, scarfyPos);
-
-
     }
-    CleanupGame(scarfy);
 }
 
 
-// Function definitions
 void InitializeGame(Texture2D &scarfy, Rectangle &scarfyRect, Vector2 &scarfyPos, const int &screenHeight, const int &screenWidth) {
     InitWindow(screenWidth, screenHeight, "Dapper Dasher");
     scarfy = LoadTexture("textures/scarfy.png");
@@ -87,15 +132,24 @@ void UpdatePlayer(Vector2 &scarfyPos, Rectangle &scarfyRect, bool &isInAir, int 
     // Update the player Y position
     scarfyPos.y += velocity * deltaTime;
 }
-void DrawGame(Texture2D scarfy, Rectangle scarfyRect, Vector2 scarfyPos) {
+void DrawGame(Texture2D scarfy, Texture2D nebula, Rectangle scarfyRect, Rectangle nebulaRect, Vector2 scarfyPos, Vector2 nebulaPos) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
+
+    // Draw the nebula
+    DrawTextureRec(nebula, nebulaRect, nebulaPos, WHITE);
+
+
+    // Draw the player
     DrawTextureRec(scarfy, scarfyRect, scarfyPos, WHITE);
+    
+    
     EndDrawing();
 }
 
-void CleanupGame(Texture2D scarfy) {
+void CleanupGame(Texture2D scarfy, Texture2D nebula) {
     UnloadTexture(scarfy);
+    UnloadTexture(nebula);
     CloseWindow();
 }
 
